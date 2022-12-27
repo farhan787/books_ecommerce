9e441e8e2e0ca8aebbb161e067cc1e39e6b28510
Muhammad Farhan <farhanshah.96930012@gmail.com>
2022-12-28 00:41:16 +0530

    Added cors

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
+app.use(cors());
 app.use(function (req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header(
